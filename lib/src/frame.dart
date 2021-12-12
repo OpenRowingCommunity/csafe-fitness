4b9e5b6c382c750a514c636a983a659e0e07663d
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-12-12 11:34:34 -0500

    split calculateChecksum into a new private function

--- lib/src/frame.dart
+++ lib/src/frame.dart
@@ -70,18 +70,20 @@ class CsafeFrame {
 
   /// validate that the checksum and frameContents are consistent with each other
   bool validate() {
+    return _calculateChecksum() == checksum;
+  }
+
+  int _calculateChecksum() {
     // 1 byte XOR of all bytes in Frame Contents. When calculating the checksum for a frame, a starting value of 0 is used.
     int calculatedChecksum = 0;
 
     if (frameContents.isEmpty) {
-      return checksum == 0;
+      return 0;
     }
 
     for (var byte in frameContents) {
       calculatedChecksum ^= byte;
     }
-    print(calculatedChecksum);
-
-    return calculatedChecksum == checksum;
+    return calculatedChecksum;
   }
 }
