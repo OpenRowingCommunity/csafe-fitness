0f5f5178864a8e8d71cbd1bec3381b82a1054447
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-12-15 16:02:53 -0500

    move enumtypes to a directory

--- lib/src/command_types.dart
+++ lib/src/command_types.dart
@@ -1,7 +1,7 @@
 import 'dart:typed_data';
 import 'package:equatable/equatable.dart';
 
-import 'enumtypes.dart';
+import 'types/enumtypes.dart';
 
 class CsafeShortCommand {
   CsafeCommandIdentifier command;
