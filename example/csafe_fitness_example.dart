94b2a9eb5359a4d745f2da3a53d44e5ecf615ec6
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-12-20 13:11:23 -0800

    update example file with some mostly-finished sample usage

--- example/csafe_fitness_example.dart
+++ example/csafe_fitness_example.dart
@@ -1,6 +1,28 @@
+import 'dart:typed_data';
+
 import 'package:csafe_fitness/csafe_fitness.dart';
+import 'package:csafe_fitness/src/types/command_types.dart';
+import 'package:csafe_fitness/src/types/datatypes.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  // Create a Csafe instance
+  Csafe csafe = Csafe();
+
+  // create a command
+
+  CsafeCommand cmd1 =
+      CsafeCommand.long(0xAB, 2, Uint8List.fromList([0xBE, 0xEF]));
+
+  // alternatively, use a command factory
+  // CsafeLongCommandFactory factory = CsafeLongCommandFactory(0xAB, placeholder);
+
+  // CsafeCommand cmd2 = factory.buildFromValue(CsafeIntegerWithUnits.meters(50));
+
+  // send the command. You get a future for when the results come back!
+  csafe.sendCommands([cmd1]).then((responseToCommands) => {
+        //your handling of responses goes here
+      });
+
+  //this future can also throw errors if the command was badly formatted or illegal. these can be handled as errors
+
 }
