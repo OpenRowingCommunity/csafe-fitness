5e4626a4a639c79c87326dfe5041dc15213f2301
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-12-23 15:20:10 -0800

    improve error message when state is not changed

--- lib/src/csafe.dart
+++ lib/src/csafe.dart
@@ -59,7 +59,8 @@ class Csafe {
       //last packet received was not ok
       // resolve the future with an error
       // TODO: turn these status types into more proper error types that are easier for users to handle
-      matchingCommand.completer.completeError(resp.status.prevState);
+      matchingCommand.completer
+          .completeError("previous command state was ${resp.status.prevState}");
     } else {
       // resolve that future successfully
       matchingCommand.completer.complete(resp);
