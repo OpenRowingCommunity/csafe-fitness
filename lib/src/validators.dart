b0033016eeeb16efbd982c07475914a2b50832cc
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-01-01 00:46:05 -0800

    add validators

---
+++ lib/src/validators.dart
@@ -0,0 +1,37 @@
+/// This file contains functions that return functions to validate that data conforms to particular requirements
+///
+/// These are used by subclasses of [CsafeCommand] for validating input
+import 'interfaces.dart';
+import 'types/enumtypes.dart';
+
+import 'types/datatypes.dart';
+
+/// Creates a validator function
+///
+/// [condition] is a function that takes a [ByteSerializable] and returns the boolean result of the validation condition
+/// [error] is an error to be thrown in the event that the condition fails and the caller wanted the validator to throw an exception with a detailed error message
+Validator validate(bool Function(ByteSerializable) condition, Error error) {
+  return (ByteSerializable data, {bool shouldThrow = false}) {
+    if (condition(data)) return true;
+    return (shouldThrow) ? throw error : false;
+  };
+}
+
+/// Shortcut for a validator to assert that the data has a specific length
+Validator validateLength(int expectedLength) {
+  return validate(
+      (data) => (data.byteLength == expectedLength), ArgumentError(""));
+}
+
+/// Shortcut for a validator to assert that the data has a specific type
+Validator validateType<T>() {
+  return validate((data) => (data is T), ArgumentError(""));
+}
+
+/// Shortcut for a validator to assert that the data is a unit with a specific type (i.e. distance, force)
+Validator validateUnitType(UnitType expectedType) {
+  return validate(
+      (data) =>
+          (data is CsafeIntegerWithUnits && data.unit.unitType == expectedType),
+      ArgumentError(""));
+}
